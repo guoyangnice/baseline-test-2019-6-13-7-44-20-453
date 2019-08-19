b96accb0f51e233cbb33d247b2494bf25f81e9d3
guoyangnice <yangjienice@126.com>
2019-08-19 17:49:41 +0800

    提交作业代码

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,17 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  // if(Object.prototype.toString.call(collectionA)=="[Object Array]" && Object.prototype.toString.call(collectionB)=="[object Array]"){
+  //     return collectionA.filter(function(v){
+
+  //       return collectionB.indexOf(v)!==-1
+  //     })
+  // }
+
+//   var result = collectionB.filter(function(v){
+//     return collectionA.indexOf(v)!==-1 // 利用filter方法来遍历是否有相同的元素
+// })
+var c = collectionA.filter(function(v){ return collectionB.indexOf(v) > -1 })
+return c
+ // return '实现练习要求，并改写该行代码。';
 }
